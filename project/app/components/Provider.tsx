2e747ad15eabb08e05e6e9dc305c681ed0fae6c1
OsamabinAdnan <binadnanosama@gmail.com>
2025-01-03 00:42:54 +0500

    provider urls updated

--- project/app/components/Provider.tsx
+++ project/app/components/Provider.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({children}:{children:React.ReactNode}) {
             mode='payment'
             cartMode='client-only'
             stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-            successUrl='http://localhost:3000/stripe/success'
-            cancelUrl='http://localhost:3000/stripe/error'
+            successUrl='https://bazariox-osamabinadnan.vercel.app/stripe/success'
+            cancelUrl='https://bazariox-osamabinadnan.vercel.app/stripe/error'
             currency='USD'
             billingAddressCollection={false}
             shouldPersist={true}
